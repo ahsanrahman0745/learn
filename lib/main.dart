2113462bf67aac7e79011deb475c75c9e0f81e3c
ahsanrahman0745 <arahman@medelz.com>
2023-06-21 21:17:52 +0500

    Initial commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+<<<<<<< HEAD
   runApp(MyApp());
 }
 
@@ -157,6 +158,66 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+=======
+  runApp(MyWelcomeScreen());
+}
+
+class MyWelcomeScreen extends StatefulWidget {
+
+  @override
+  State<MyWelcomeScreen> createState() => _MyWelcomeScreenState();
+}
+
+
+class _MyWelcomeScreenState extends State<MyWelcomeScreen>{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton.small(onPressed: (){},child:  Icon(Icons.plus_one,color: Colors.white,),),
+          appBar: AppBar(
+            automaticallyImplyLeading: true,
+            title: Text("Page Title",style: TextStyle(fontSize: 20,color: Colors.white),),
+            actions: [
+              Icon(Icons.favorite,color: Colors.white,),
+              Icon(Icons.search,color: Colors.white,),
+              Icon(Icons.messenger_sharp,color: Colors.white,),
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                DrawerHeader(child: Column(
+                  children: [
+                    Image.asset("assets/background/background.png",
+                    width: double.infinity,
+                    height: 80,),
+                    Text("User Name",style: TextStyle(color: Colors.black,fontSize: 22,fontWeight: FontWeight.bold ),),
+                    Text("Email",style: TextStyle(color: Colors.black,fontSize: 22,fontWeight: FontWeight.bold ),)
+                  ],
+                )),
+                ListTile(
+                  tileColor: Colors.lightBlue,
+                  leading: Icon(Icons.person,color: Colors.white,),
+                  title: Text("My Account",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold ),),
+                  trailing: Icon(Icons.arrow_right_alt,color: Colors.white,),
+                ),
+                ListTile(
+                  tileColor: Colors.lightBlue,
+                  leading: Icon(Icons.key,color: Colors.white,),
+                  title: Text("change Passwrod",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold ),),
+                  trailing: Icon(Icons.arrow_right_alt,color: Colors.white,),
+                ),
+              ],
+            ),
+          ),
+          body: Text("test 1",style: TextStyle(fontSize: 25) )),
+      
+    );
+  }
+
+>>>>>>> Initial commit 2
 }
 
 
@@ -166,3 +227,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> Initial commit 2
